0343f697160d83152f829f2c40dbe4d414424dc7
raza11409652 <raza.11409652@lpu.in>
2020-03-12 19:59:54 +0530

    Order Page

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -62,7 +62,6 @@ class _OrderPageState extends State<OrderPage> {
 
 
  Widget _buildTotalContainer() {
-
     return Container(
       child: Padding(
         padding: EdgeInsets.all(10.0),
@@ -76,13 +75,13 @@ class _OrderPageState extends State<OrderPage> {
             ) , 
             ListTile(
               dense: true,
-               contentPadding: EdgeInsets.zero ,
+              contentPadding: EdgeInsets.zero ,
               leading: Text("Discount"),
               trailing: Text("1.0"),
-            ) , 
+              ) , 
              ListTile(
-               dense: true,
-                contentPadding: EdgeInsets.zero ,
+              dense: true,
+              contentPadding: EdgeInsets.zero ,
               leading: Text("Discount"),
               trailing: Text("1.0"),
             ) , 
@@ -105,11 +104,29 @@ class _OrderPageState extends State<OrderPage> {
                 Navigator.of(context).push(
                   MaterialPageRoute(builder: (BuildContext ctx)=>SignIn())
                 ) ; 
+                // _transaction = initiateTransaction(UpiIndiaApps.AmazonPay);
               },
               textColor: Colors.white,
             color: Colors.redAccent,
             child: Text("Proceed to checkout"),
-            )  , 
+            )  ,
+            GestureDetector(
+              onTap: (){
+                _showBottomSheet();
+              },
+              child: Container(
+                height: 50.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(45.0),
+                  color: Colors.redAccent
+                ),
+                child: Center(
+                  child:Text("Show bottom navigation",style: TextStyle(
+                    color: Colors.white
+                  ),)
+                  ),
+              ),
+            ) 
             // transaction(isTransaction)
             ],
         ),
@@ -119,6 +136,43 @@ class _OrderPageState extends State<OrderPage> {
     );
   }
 
+
+Column _buildBottomSheet(){
+ return Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.visibility),
+          title: Text("Hello"),
+          onTap: (){
+              Navigator.pop(context);
+          },
+        )  ,
+        ListTile(
+          leading: Icon(Icons.home),
+          title: Text("Option 2"),
+        )
+      ],
+    ) ;
+}
+void _showBottomSheet(){
+  showModalBottomSheet(context: context, 
+  builder: (context){
+    return Container(
+     color: Color(0xFF737373),
+     child: Container(
+        decoration: BoxDecoration(
+        color: Theme.of(context).canvasColor,
+        borderRadius: BorderRadius.only(
+          topLeft: const Radius.circular(10),
+          topRight: const Radius.circular(10)
+        )
+      ),
+      child: _buildBottomSheet(),
+     ),
+    ) ;
+   
+  }) ; 
+}
  
     
   
